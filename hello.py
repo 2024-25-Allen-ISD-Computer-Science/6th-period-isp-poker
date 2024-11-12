447ddf0c37eba1274b3bc314a591a8f53550b3f4
andrew-m <aamaribe@gmail.com>
2024-11-11 21:51:59 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello World!");
+import tkinter as tk
+
+#Make window
+window = tk.Tk()
+window.title("poker window test")
+window.geometry("300x200")  #Window size
+
+# Set the background color and font color
+window.configure(bg="blue")
+
+#Make text
+label = tk.Label(window, text="window test", font=("Arial", 20), fg="red", bg="blue")
+label.pack(expand=True)  #Center the label
+
+# Run the application
+window.mainloop()
